406a04ab55bc02f33d6feb0222def8477749c936
Rumen Guin <rumen2k@gmail.com>
2023-01-28 20:15:23 +0530

    Initial Commit

---
+++ BlingCloud/BlingCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlingCloudApp.swift
+//  BlingCloud
+//
+//  Created by RUMEN GUIN on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BlingCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
